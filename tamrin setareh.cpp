ba5410442610f2b9151664118e0fed9b48bafbf5
ghazal-hup <g67545583@gmail.com>
2024-12-04 00:38:15 +0330

    Add files via upload

---
+++ tamrin setareh.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace  std;
+ int main (){
+ 	int i;
+	for(i=1;i<=5;i++);
+	cout<<"*"<<"\n"<<"**"<<"\n"<<"***"<<"\n"<<"****";
+}
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
